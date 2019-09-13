59753d066b53413db12da096f8187a24813efea0
wlferreirapucminas <49538726+wlferreirapucminas@users.noreply.github.com>
2019-09-13 17:21:38 -0300

    Delete IgrejaItem.cs

--- src/Gem.Api/Models/IgrejaItem.cs
+++
@@ -1,9 +0,0 @@
-namespace GemApi.Models
-{
-    public class IgrejaItem
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public bool Status { get; set; }
-    }
-}
