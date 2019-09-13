2fbf05b6791663825ed532bebb514574fd48d937
wlferreirapucminas <49538726+wlferreirapucminas@users.noreply.github.com>
2019-09-13 17:18:58 -0300

    Add files via upload

---
+++ src/Gem.Api/Migrations/20190913040712_ModelosEF.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gem.Api.Migrations
+{
+    public partial class ModelosEF : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EscolaItens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EscolaItens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IgrejaItens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IgrejaItens", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EscolaItens");
+
+            migrationBuilder.DropTable(
+                name: "IgrejaItens");
+        }
+    }
+}
