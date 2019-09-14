efbec1112be49c9d8791828d38409d6cd083682a
wlferreirapucminas <49538726+wlferreirapucminas@users.noreply.github.com>
2019-09-14 10:36:17 -0300

    Add files via upload

---
+++ src/Gem.Api/Models/MinisterioItem.cs
@@ -0,0 +1,9 @@
+namespace GemApi.Models
+{
+    public class MinisterioItem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Status { get; set; }
+    }
+}
