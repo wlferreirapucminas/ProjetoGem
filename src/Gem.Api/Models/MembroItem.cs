556a29f761026e336038313dc8b2d1c05d488b36
wlferreirapucminas <49538726+wlferreirapucminas@users.noreply.github.com>
2019-09-13 21:46:15 -0300

    Add files via upload

---
+++ src/Gem.Api/Models/MembroItem.cs
@@ -0,0 +1,9 @@
+namespace GemApi.Models
+{
+    public class MembroItem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Status { get; set; }
+    }
+}
